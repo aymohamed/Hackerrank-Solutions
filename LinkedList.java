3f5988851918f6e24a00c5a6555f1f7375c6f481
Abdijabar <aymohamed@middlebury.edu>
2017-03-30 20:11:00 -0400

    added functionality of the LinkedList methods

---
+++ LinkedList.java
@@ -0,0 +1,68 @@
+/*
+  Insert Node at the beginning of a linked list
+  head pointer input could be NULL as well for empty list
+  Node is defined as
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+// These methods shown below are "method-only" submission.
+// You only need to complete these methods.
+
+
+Node Insert(Node head,int x) {
+    Node mynode = new Node();
+    mynode.data = x;
+    mynode.next = head;
+    return mynode;
+}
+
+
+
+/*
+  Insert Node at the end of a linked list
+  head pointer input could be NULL as well for empty list
+
+*/
+Node Insert(Node head,int data) {
+
+    if(head==null){
+        head = new Node();
+        head.data = data;
+
+    } else{
+        Node current = head;
+        while(current.next!=null){
+        current = current.next;
+    }
+    current.next = new Node();
+    current.next.data = data;
+    }
+    return head;
+
+
+
+
+}
+
+
+/*
+  Print elements of a linked list
+  head pointer input could be NULL as well for empty list
+
+*/
+
+
+void Print(Node head) {
+    if(head==null){
+        return;
+    }
+    Node temp = head;
+    while(temp!=null){
+        System.out.println(temp.data);
+        temp=temp.next;
+    }
+
+
+}
